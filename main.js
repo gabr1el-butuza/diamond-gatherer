fdd4546d2bd86f62bfb7445bcd0b815c97d52dcf
gabr1el-butuza <gabriel.butuza97@gmail.com>
2020-11-14 18:59:22 +0200

    TEMA 1

---
+++ main.js
@@ -0,0 +1,13 @@
+var car = {
+    type: "Ford",
+    model: "Mustang",
+    color: "Yellow",
+}
+
+console.log("type: " + car.type + "\nmodel: " + car.model + "\ncolor: " + car.color);
+
+function greeting(nume, prenume) {
+    return "Buna, numele meu este " + nume + " " + prenume;
+}
+
+console.log(greeting("Butuza", "Gabriel"));
