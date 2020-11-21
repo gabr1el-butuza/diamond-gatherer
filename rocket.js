8253e5889ff52f2d0b162bdea108a12b7fe03b85
gabr1el-butuza <gabriel.butuza97@gmail.com>
2020-11-21 20:02:03 +0200

    TEMA CURS 2

--- rocket.js
+++ rocket.js
@@ -21,10 +21,8 @@ export class Rocket {
     land() {
         if (this.remainingGas > 1000 && this.cargoWeight < (40 / 100) * this.weight) {
             console.log(`Rocket ${this.name} land successfully!`);
-            return true;
         } else {
             console.log(`Red Alert! The fuel is not enough or the cargo is too heavy.`);
-            return false;
         }
 
 
